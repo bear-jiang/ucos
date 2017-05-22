486dad7920037305cf370c97c3cfc1e776e75402
Bear.Jiang <2320767099@qq.com>
2017-05-22 16:18:24 +0800

    First commit;Can run

---
+++ app/app.c
@@ -0,0 +1,174 @@
+/*
+*********************************************************************************************************
+*                                              EXAMPLE CODE
+*
+*                             (c) Copyright 2013; Micrium, Inc.; Weston, FL
+*
+*                   All rights reserved.  Protected by international copyright laws.
+*                   Knowledge of the source code may not be used to write a similar
+*                   product.  This file may only be used in accordance with a license
+*                   and should not be redistributed in any way.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                            EXAMPLE CODE
+*
+*                                       IAR Development Kits
+*                                              on the
+*
+*                                    STM32F429II-SK KICKSTART KIT
+*
+* Filename      : app.c
+* Version       : V1.00
+* Programmer(s) : YS
+*                 DC
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                             INCLUDE FILES
+*********************************************************************************************************
+*/
+
+#include  <includes.h>
+// #include  <task.h>
+/*
+*********************************************************************************************************
+*                                            LOCAL DEFINES
+*********************************************************************************************************
+*/
+
+#define  APP_TASK_EQ_0_ITERATION_NBR              16u
+
+
+/*
+*********************************************************************************************************
+*                                       LOCAL GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+                                                                /* ----------------- APPLICATION GLOBALS -------------- */
+static  OS_TCB   AppTaskStartTCB;
+static  CPU_STK  AppTaskStartStk[APP_CFG_TASK_START_STK_SIZE];
+                                                                /* ------------ FLOATING POINT TEST TASK -------------- */
+
+
+/*
+*********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+static  void  AppTaskStart          (void     *p_arg);
+
+/*
+*********************************************************************************************************
+*                                                main()
+*
+* Description : This is the standard entry point for C code.  It is assumed that your code will call
+*               main() once you have performed all necessary initialization.
+*
+* Arguments   : none
+*
+* Returns     : none
+*********************************************************************************************************
+*/
+
+int main(void)
+{
+    int a;
+    OS_ERR  err;
+
+
+    BSP_IntDisAll();                                            /* Disable all interrupts.                              */
+    
+    CPU_Init();                                                 /* Initialize the uC/CPU Services                       */
+    Mem_Init();                                                 /* Initialize Memory Management Module                  */
+    Math_Init();                                                /* Initialize Mathematical Module                       */
+
+    OSInit(&err);                                               /* Init uC/OS-III.                                      */
+
+    OSTaskCreate((OS_TCB       *)&AppTaskStartTCB,              /* Create the start task                                */
+                 (CPU_CHAR     *)"App Task Start",
+                 (OS_TASK_PTR   )AppTaskStart,
+                 (void         *)0u,
+                 (OS_PRIO       )APP_CFG_TASK_START_PRIO,
+                 (CPU_STK      *)&AppTaskStartStk[0u],
+                 (CPU_STK_SIZE  )AppTaskStartStk[APP_CFG_TASK_START_STK_SIZE / 10u],
+                 (CPU_STK_SIZE  )APP_CFG_TASK_START_STK_SIZE,
+                 (OS_MSG_QTY    )0u,
+                 (OS_TICK       )0u,
+                 (void         *)0u,
+                 (OS_OPT        )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+                 (OS_ERR       *)&err);
+
+    OSStart(&err);                                              /* Start multitasking (i.e. give control to uC/OS-III). */
+
+    (void)&err;
+
+    return (0u);
+}
+
+
+/*
+*********************************************************************************************************
+*                                          STARTUP TASK
+*
+* Description : This is an example of a startup task.  As mentioned in the book's text, you MUST
+*               initialize the ticker only once multitasking has started.
+*
+* Arguments   : p_arg   is the argument passed to 'AppTaskStart()' by 'OSTaskCreate()'.
+*
+* Returns     : none
+*
+* Notes       : 1) The first line of code is used to prevent a compiler warning because 'p_arg' is not
+*                  used.  The compiler should not generate any code for this statement.
+*********************************************************************************************************
+*/
+
+static  void  AppTaskStart (void *p_arg)
+{
+    OS_ERR  err;
+
+   (void)p_arg;
+
+    BSP_Init();                                                 /* Initialize BSP functions                             */
+    BSP_Tick_Init();                                            /* Initialize Tick Services.                            */
+
+   
+
+#if OS_CFG_STAT_TASK_EN > 0u
+    OSStatTaskCPUUsageInit(&err);                               /* Compute CPU capacity with no task running            */
+#endif
+
+#ifdef CPU_CFG_INT_DIS_MEAS_EN
+    CPU_IntDisMeasMaxCurReset();
+#endif
+    APP_TRACE_DBG(("Creating Application Kernel Objects\n\r"));
+    // SensorTaskCreate();                                             /* Create Applicaiton kernel objects                    */
+
+    APP_TRACE_DBG(("Creating Application Tasks\n\r"));
+
+    while (DEF_TRUE) {                                          /* Task body, always written as an infinite loop.       */
+        LED_GREEN_ON();
+        // LED_RED_OFF();
+        OSTimeDlyHMSM(0u, 0u, 0u, 500u,
+                  OS_OPT_TIME_HMSM_STRICT,
+                  &err);
+        // usart1_send_string("hhh jjj%d",-100);
+        // LED_RED_ON();
+        USART1_Send(0xee);
+        LED_GREEN_OFF();
+        OSTimeDlyHMSM(0u, 0u, 0u, 500u,
+                  OS_OPT_TIME_HMSM_STRICT,
+                  &err);
+    }
+}
+
+
+
+
